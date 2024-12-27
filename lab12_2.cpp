d1c015b0c1fc7d746bb235be038fec70691f39ab
Sorakrit05 <aomsin22122558@gmail.com>
2024-12-27 13:36:43 +0700

    lab12.2.1

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int, int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    while(x%y != 0){
+       x = x%y;
+       return gcd(y,x);
+    }
+    return y;
 }
